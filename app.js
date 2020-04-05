d5e2302764505b78074da0626a3eed9ea3dc1a79
Kagabu Isa <kagabuisa@gmail.com>
2020-04-05 15:56:52 +0300

    05-04 commit

---
+++ app.js
@@ -0,0 +1,22 @@
+let name = "Kagabu Isa"
+console.log(name);
+let catherine = "accountant"
+
+function cate(){
+    return `${name} ${catherine}`
+}
+
+cate()
+
+const runnerx = {
+    first: 'Isakale',
+    last: 'Kagabu',
+    country: 'Uganda',
+    title: 'Hajji'
+}
+ const now = get(Date)
+function print({first,last,country}){
+//const {first,last} = greoner
+    console.log(`${first}  ${last}, of the country ${country} in East Africa ${new date()}`)
+}
+
