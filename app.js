bfc455f787389963c52e1ad2b10eac4f9a22d563
Kagabu Isa <kagabuisa@gmail.com>
2020-04-19 20:26:16 +0300

    commit 19-04-2020

--- app.js
+++ app.js
@@ -1,144 +1,126 @@
-let name = "Kagabu Isa"
-console.log(name);
-let catherine = "accountant"
-
-function cate() {
-    return `${name} ${catherine}`
-}
-
-cate()
-
-const runnerx = {
-    first: 'Isakale',
-    last: 'Kagabu',
-    country: 'Uganda',
-    title: 'Hajji'
-}
-const now = '05-04-2020'
-
-function print({
-    first,
-    last,
-    country
-}) {
-    //const {first,last} = greoner
-    console.log(`${first}  ${last}, of the country ${country} in East Africa ${new date()}`)
-}
-
-const grades = [4.2, 4.8, 3.3, 4.4, 2.5, 3.2, 3.1, 4.9]
-const getStats = (arr) => {
-    const min = (Math.min(...grades));
-    const max = (Math.max(...grades));
-    const avg = (grades.reduce((sum, r) => {
-        return sum + r
-    })) / grades.length;
-    const sum = (grades.reduce((sum, r) => {
-        return sum + r
-    }));
-    return {
-        sum,
-        max,
-        min: min,
-        avg: avg
-    }
-}
-
-console.log(getStats())
-
-const role = 'Host';
-const person = 'Joona Holland';
-const role2 = 'Director';
-const person2 = 'James Cameroon';
-
-const team = {
-    [role]: person,
-    [role2]: person2
-}
-
-console.log(team, team * 2)
-
-//method add,subtract,divide,multiply
-
-
-const math = {
-
-    add(x, y) {
-        return x + y
-    },
-
-    subtract(x, y) {
-        return x - y
-    },
-
-    divide(x, y) {
-        return x / y
-    },
-
-    multiply(x, y) {
-        return x * y
-    },
-    fullList() {
-        console.log(this)
-    }
-
-}
-
-const personName = {
-    firstName: 'Kibuka',
-    lastName: 'Mohan',
+const persona = {
+    first: 'Busingye',
+    last: 'Salama',
+    nickName: 'Sera',
     fullName() {
         const {
-            firstName,
-            lastName
+            first,
+            last,
+            nickName
         } = this
-        console.log(firstName)
+        return `${first} ${last} AKA ${nickName}`
+    },
+    printBio() {
+        console.log(this);
+        const fullName = this.fullName();
+        console.log(`${fullName} is a person`)
+    }
+}
+const printBot = persona.printBio;
+
+function makeDeck() {
+    const deck = []
+
+}
+
+
+//functionality 1.Draw 2.Shuffle
+
+const myDeck = {
+    deck: [],
+    drawnCards: [],
+    suits: ['hearts', 'spades', 'diamonds', 'clubs'],
+    values: '2,3,4,5,6,7,8,9,0,J,Q,K,A',
+    initializeDeck() {
+        const {
+            suits,
+            values,
+            deck
+        } = this;
+        for (let value of values.split(',')) {
+            for (let suit of suits) {
+                deck.push({
+                    value: value,
+                    suit: suit
+                })
+            }
+        }
+    },
+    drawCard() {
+        const cardDrawn = this.deck.pop()
+        this.drawnCards.push(cardDrawn)
+        return cardDrawn
+    },
+    drawMultiplecard(numofCards) {
+        multiDraws = []
+        for (let i = 0; i < numofCards; i++) {
+            multiDraws.push(this.drawCard())
+        }
+        return multiDraws;
+    },
+    shuffleCards() {
+        const {
+            deck
+        } = this
+        const vid = Math.floor(Math.random(deck.length) * deck.length)
+        console.log(vid)
+        for (let i = deck.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [deck[i], deck[j]] = [deck[j], deck[i]]
+        }
     }
 }
 
-//   var date = new Date();
-//   let curDate = date.getDate()
-//   let curYear = date.getFullYear()
-// var nextDate = date.getDate() + 20;
-// date.setDate(nextDate);
-// var newDate = date.toLocaleString();
-
-// let doReport = {
-//     "action": {
-//       "id": 911899464,
-//       "status": "in-progress",
-//       "type": "snapshot",
-//       "started_at": "2020-04-08T13:16:05Z",
-//       "completed_at":""
-//     }
-//   }
-// var date1 = new Date("July 19, 2014");
-// var date2 = new Date(doReport.action.started_at.slice(0,10));
-// let diffDate= Math.floor((date-date2)/(1000*60*60*24))/365
-// let today = Date.now()
-// let newCurDate = Math.floor(date.date2()+10);
-
-function getSnapshotList() {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer b55f0a04ae134281f185d86f0e2f5456baa43a6dfddb7edc3cbf30a5877c1985");
-    myHeaders.append("Cookie", "__cfduid=dcea941712d2e0d1fbac975de663b92ac1586247452");
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-    };
-
-    fetch("https://api.digitalocean.com/v2/snapshots?page=1&per_page=1&resource_type=droplet", requestOptions)
-    .then(response => response.json())
-    //.then(response => response.json())
- 
-        .then(result => console.log(result))
-        .then(success => {
-            const gotSnapshots = response.json()
-            return gotSnapshots
-        })
-        .catch(error => console.log('error', error));
-
+function dealCards(NOD) {
+    myDeck.initializeDeck()
+    myDeck.shuffleCards()
+    myDeck.drawMultiplecard(NOD)
+    console.log(myDeck.deck)
 }
 
-getSnapshotList(); tangawiz, nimu, garlic
+function makeDeck(){
+    return {
+            deck: [],
+            drawnCards: [],
+            suits: ['hearts', 'spades', 'diamonds', 'clubs'],
+            values: '2,3,4,5,6,7,8,9,0,J,Q,K,A',
+            initializeDeck() {
+                const {
+                    suits,
+                    values,
+                    deck
+                } = this;
+                for (let value of values.split(',')) {
+                    for (let suit of suits) {
+                        deck.push({
+                            value: value,
+                            suit: suit
+                        })
+                    }
+                }
+            },
+            drawCard() {
+                const cardDrawn = this.deck.pop()
+                this.drawnCards.push(cardDrawn)
+                return cardDrawn
+            },
+            drawMultiplecard(numofCards) {
+                multiDraws = []
+                for (let i = 0; i < numofCards; i++) {
+                    multiDraws.push(this.drawCard())
+                }
+                return multiDraws;
+            },
+            shuffleCards() {
+                const {
+                    deck
+                } = this
+                const vid = Math.floor(Math.random(deck.length) * deck.length)
+                console.log(vid)
+                for (let i = deck.length - 1; i > 0; i--) {
+                    let j = Math.floor(Math.random() * (i + 1));
+                    [deck[i], deck[j]] = [deck[j], deck[i]]
+                }
+            }
+        }
+    }
